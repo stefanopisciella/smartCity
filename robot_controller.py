b5d705afb3941840d4e3df57eedd88bf90780955
stefanopisciella <stefano.pisciella1@student.univaq.it>
2023-12-19 19:51:48 +0100

    aruco marker detection and camera working

--- robot_controller.py
+++ robot_controller.py
@@ -5,7 +5,8 @@ import cv2
 
 
 robot = Robot()
-camera = Camera('camera')
+camera = robot.getDevice('camera')
+# camera = Camera('camera')
 
 # MOTORS
 front_left_motor = Motor('front left wheel')
@@ -22,20 +23,13 @@ front_right_motor.setPosition(float('inf'))
 back_left_motor.setPosition(float('inf'))
 back_right_motor.setPosition(float('inf'))
 
-"""
-front_left_motor.setVelocity(0.0)
-front_right_motor.setVelocity(0.0)
-back_left_motor.setVelocity(0.0)
-back_right_motor.setVelocity(0.0)
-"""
-
-
-
-
 ###
 arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
 arucoParams = cv2.aruco.DetectorParameters()
 ###
+
+front_left_motor_speed = 0.1 * MAX_SPEED
+front_right_motor_speed = 0.1 * MAX_SPEED
 
 while robot.step(TIMESTAMP) != -1:
     camera.enable(TIMESTAMP)
@@ -59,9 +53,6 @@ while robot.step(TIMESTAMP) != -1:
     ###
 
     # START control the PIONEER
-    front_left_motor_speed = 1 * MAX_SPEED
-    front_right_motor_speed = 1 * MAX_SPEED
-
     front_left_motor.setVelocity(front_left_motor_speed)
     front_right_motor.setVelocity(front_right_motor_speed)
     # END
